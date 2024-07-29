aa387b0586526f056d5f11a54a17cb4208dad693
kushallegit <169062369+kushallegit@users.noreply.github.com>
2024-07-29 10:54:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var itemCount = 0;
+
+function addToList(){
+    itemCount++;
+    var listItem = document.createElement('li');
+    listItem.innerText = 'Item' + itemCount;
+    document.getElementById('itemList').appendChild(listItem);
+}
